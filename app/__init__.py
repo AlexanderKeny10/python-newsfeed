a5101b13e632b864c45257cfe5ae019a282160f0
AKen00 <ajken04@gmail.com>
2021-07-28 08:44:30 -0400

    Finalize homepage views start

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from app.routes import home, dashboard
+from flask import Flask
+from app.db import init_db
+
+def create_app(test_config=None):
+  # set up app config
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+  app.config.from_mapping(
+    SECRET_KEY='super_secret_key'
+  )
+
+#   @app.route('/hello')
+#   def hello():
+#       return 'hello world'
+
+  init_db(app)
+
+  return app
